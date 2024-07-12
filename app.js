1bf8af47c163d71a5220b7aedc25c29585820084
Thiago Lima <Ribeiro7380@gmail.com>
2024-07-12 17:28:57 -0300

    finalizando projeto

--- app.js
+++ app.js
@@ -3,28 +3,33 @@ var outInput = document.querySelector("#output");
 
 
 function criptografar() {
-
   var texto = textInput.value;
 
-  var resultCripto = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+  var resultCripto = texto.replace(/e/gi, "enter")
+  .replace(/i/gi, "imes")
+  .replace(/a/gi, "ai")
+  .replace(/o/g, "ober")
+  .replace(/u/g, "ufat")
 
-
-  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto + 
+  document.getElementById('output').innerHTML = '<textarea readonly id="result">' + resultCripto + 
   '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function descriptografar() {
-
     var texto = textInput.value;
 
-    var resultDescripto = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    var resultDescripto = texto.replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
 
-    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
+    document.getElementById('output').innerHTML = '<textarea readonly id="result">' + resultDescripto + 
     '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function copiar() {
-    var textoCop = document.getElementById('input-texto');
+    var textoCop = document.getElementById('result');
 
     textoCop.select();
     document.execCommand('copy');
